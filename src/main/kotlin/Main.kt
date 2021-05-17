c542a2539d14e07344e0c94dd3132211270c6cc4
Alex Plate <aleksei.plate@jetbrains.com>
2021-05-17 16:08:12 +0300

    Wrap with class

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,58 +7,60 @@ import org.intellij.markdown.parser.MarkdownParser
 import org.kohsuke.github.GitHub
 import java.io.File
 
-fun main(args: Array<String>) {
-    val path = args[0]
-    val token = args[1]
-    val repository = RepositoryBuilder().setGitDir(File("$path/.git")).build()
-    val git = Git(repository)
-    val emails = git.log().call().take(20).mapTo(HashSet()) { it.authorIdent.emailAddress }
+class UpdateAuthors {
+    fun update(path: String, token: String) {
+        val repository = RepositoryBuilder().setGitDir(File("$path/.git")).build()
+        val git = Git(repository)
+        val emails = git.log().call().take(20).mapTo(HashSet()) { it.authorIdent.emailAddress }
 
-    val gitHub = GitHub.connect()
-    val searchUsers = gitHub.searchUsers()
-    val users = mutableListOf<Author>()
-    for (email in emails) {
-        if (email == "aleksei.plate@jetbrains.com") continue
-        val user = searchUsers.q(email).list().single()
-        val htmlUrl = user.htmlUrl.toString()
-        val name = user.name
-        users.add(Author(name, htmlUrl, email))
-    }
-
-    val authorsFile = File("$path/AUTHORS.md")
-    val authors = authorsFile.readText()
-    val parser = MarkdownParser(GFMFlavourDescriptor())
-    val tree = parser.buildMarkdownTreeFromString(authors)
-
-    val contributorsSection = tree.children[24]
-    val existingEmails = mutableSetOf<String>()
-    for (child in contributorsSection.children) {
-        if (child.children.size > 1) {
-            existingEmails.add(child.children[1].children[0].children[2].children[2].getTextInNode(authors).toString())
+        val gitHub = GitHub.connect()
+        val searchUsers = gitHub.searchUsers()
+        val users = mutableListOf<Author>()
+        for (email in emails) {
+            if (email == "aleksei.plate@jetbrains.com") continue
+            val user = searchUsers.q(email).list().single()
+            val htmlUrl = user.htmlUrl.toString()
+            val name = user.name
+            users.add(Author(name, htmlUrl, email))
         }
+
+        val authorsFile = File("$path/AUTHORS.md")
+        val authors = authorsFile.readText()
+        val parser = MarkdownParser(GFMFlavourDescriptor())
+        val tree = parser.buildMarkdownTreeFromString(authors)
+
+        val contributorsSection = tree.children[24]
+        val existingEmails = mutableSetOf<String>()
+        for (child in contributorsSection.children) {
+            if (child.children.size > 1) {
+                existingEmails.add(
+                    child.children[1].children[0].children[2].children[2].getTextInNode(authors).toString()
+                )
+            }
+        }
+
+        val newAuthors = users.filterNot { it.mail in existingEmails }
+        if (newAuthors.isEmpty()) return
+
+        val insertionString = newAuthors.toMdString()
+        val resultingString = StringBuffer(authors).insert(contributorsSection.endOffset, insertionString).toString()
+
+        authorsFile.writeText(resultingString)
+
+        git.add().addFilepattern(".").call()
+        val allNewAuthors = newAuthors.joinToString(", ") { it.name }
+
+        val commit = git.commit()
+        commit.setSign(false)
+        commit.setAuthor("Alex Plate", "aleksei.plate@jetbrains.com")
+        commit.message = "Add $allNewAuthors to contributors list"
+        commit.call()
+
+        val push = git.push()
+        push.remote = "https://github.com/JetBrains/ideavim"
+        push.setCredentialsProvider(UsernamePasswordCredentialsProvider(token, ""))
+        push.call()
     }
-
-    val newAuthors = users.filterNot { it.mail in existingEmails }
-    if (newAuthors.isEmpty()) return
-
-    val insertionString = newAuthors.toMdString()
-    val resultingString = StringBuffer(authors).insert(contributorsSection.endOffset, insertionString).toString()
-
-    authorsFile.writeText(resultingString)
-
-    git.add().addFilepattern(".").call()
-    val allNewAuthors = newAuthors.joinToString(", ") { it.name }
-
-    val commit = git.commit()
-    commit.setSign(false)
-    commit.setAuthor("Alex Plate", "aleksei.plate@jetbrains.com")
-    commit.message = "Add $allNewAuthors to contributors list"
-    commit.call()
-
-    val push = git.push()
-    push.remote = "https://github.com/JetBrains/ideavim"
-    push.setCredentialsProvider(UsernamePasswordCredentialsProvider(token, ""))
-    push.call()
 }
 
 fun List<Author>.toMdString(): String {
